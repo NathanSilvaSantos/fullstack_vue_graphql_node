d5e04c0ac07b3b621e2080d7792613381e9c4ec8
NathanSilvaSantos <nathan013silva@gmail.com>
2020-04-28 17:30:59 -0300

    Diretivas

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,24 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group" v-for="prefix in prefixes" v-bind:key="prefix">
-                  <li class="list-group-item"> {{ prefix }} </li>
+                  <li class="list-group-item">
+                    <div class="row">
+                      <div class="col-md">
+                        {{ prefix }}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deletePrefix(prefix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br>
-                <input type="text" class="form-control" placeholder="Digite o prefixo">
+                <div class="input-group">
+                  <input type="text" class="form-control" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" placeholder="Digite o prefixo">
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addPrefix(prefix)" ><span class="fa fa-plus"></span></button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -25,10 +39,24 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group" v-for="sufix in sufixes" v-bind:key="sufix">
-                  <li class="list-group-item"> {{sufix}} </li>
+                  <li class="list-group-item">
+                    <div class="row">
+                      <div class="col-md">
+                        {{ sufix }}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deleteSufix(sufix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br>
-                <input type="text" class="form-control" placeholder="Digite o sufixo">
+                <div class="input-group">
+                  <input type="text" class="form-control" v-model="sufix" v-on:keyup.enter="addSufix(sufix)" placeholder="Digite o prefixo">
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addSufix(sufix)"><span class="fa fa-plus"></span></button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -52,15 +80,50 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: 'App',
-  data() {
-    return {
-      prefixes: ['Air', 'Jet', 'Flight'],
-      sufixes: ['Hub', 'Station', 'Fly'],
-      domains: ['AirStation', 'JetFly', 'FlightHub']
+	name: "App",
+	data() {
+		return {
+			prefix: "",
+			sufix: "",
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Fly"],
+			domains: []
+		};
+	},
+	methods: {
+		addPrefix(prefix){
+			this.prefixes.push(prefix);
+      this.prefix = "";
+      this.generate();
+    },
+    
+    deletePrefix(prefix) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+      this.generate();
+    },
+
+		addSufix(sufix){
+			this.sufixes.push(sufix);
+      this.sufix = "";
+      this.generate();
+    },
+
+    deleteSufix(sufix) {
+      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+      this.generate();
+    },
+    
+    generate() {
+      this.domains = [];
+
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          this.domains.push(prefix + sufix);  
+        }
+      }
     }
-  }
-}
+	}
+};
 </script>
 
 <style>
